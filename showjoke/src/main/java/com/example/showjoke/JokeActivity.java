017c9e9052011b16336861edd7a7f143c49b4405
Thanawat Hanthong <peet6015@gmail.com>
2016-05-28 21:49:27 +0700

    create android  library to that will show joke in intent on screen

---
+++ showjoke/src/main/java/com/example/showjoke/JokeActivity.java
@@ -0,0 +1,21 @@
+package com.example.showjoke;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JokeActivity extends AppCompatActivity {
+
+    final String JOKE_KEY = "joke";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        Intent intent = getIntent();
+
+        TextView textView = (TextView)findViewById(R.id.text_joke);
+        textView.setText(intent.getStringExtra(JOKE_KEY));
+    }
+}
